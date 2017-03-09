eaddde3e86a3b7539c07b5501177a2df37c3d4b9
Rathish <kumar.rathish4@gmail.com>
2017-03-09 22:04:43 +0530

    added new class

---
+++ src/test/java/com/gitdemo/FirstTest.java
@@ -0,0 +1,12 @@
+package com.gitdemo;
+
+import org.testng.annotations.Test;
+
+public class FirstTest {
+
+	@Test
+	public void testFirst(){
+		System.out.println("this is my first test method");
+	}
+	
+}
