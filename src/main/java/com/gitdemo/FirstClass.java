0f243e74f4f224749d234ecc43e8789ae859f1bc
Rathish <kumar.rathish4@gmail.com>
2017-03-09 22:08:21 +0530

    changed in branch

--- src/main/java/com/gitdemo/FirstClass.java
+++ src/main/java/com/gitdemo/FirstClass.java
@@ -5,7 +5,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 		
 		System.out.println("first push");
-		System.out.println("line added ");
+		System.out.println("added to branch ");
 
 	}
 
